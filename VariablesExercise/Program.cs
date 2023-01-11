15f117936b8084b1182a13a39177614b09d90192
Jon Claxton <speedyclax@gmail.com>
2023-01-11 15:33:12 -0600

    Completed Variables Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//Declare and Initialize Variables
+string name = "Buddy";
+int age = 2;
+char gender = 'M';
+bool isPottytrained = true;
+double currentTemp = 74.3;
+decimal currentPrice = 250.50m;
+
+Console.WriteLine($"Come visit us at Pets R Us. The weather today is {currentTemp}, and we have an adorable puppy named {name} waiting to play with you.");
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Gender: {gender}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Potty Trained?: {isPottytrained}");
+Console.WriteLine($"Price: ${currentPrice}");
